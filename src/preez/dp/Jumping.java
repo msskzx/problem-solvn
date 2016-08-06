c87d8df6ab8e0e10d2694a8e01ba6d578609733f
Muhammad Khattab <msskmohammed96@gmail.com>
2016-08-06 22:16:58 +0300

    Jumping
    Find the minimum number of jumps to reach the end of the array starting
    from first element

---
+++ src/preez/dp/Jumping.java
@@ -0,0 +1,46 @@
+package preez.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Jumping {
+
+	/*
+	 * max number of steps that can be made forward from that element. Write a
+	 * function to return the minimum number of jumps to reach the end of the
+	 *
+	 * http://www.geeksforgeeks.org/minimum-number-of-jumps-to-reach-end-of-a-
+	 * given-array/
+	 * https://github.com/mission-peace/interview/blob/master/src/com/interview/
+	 * dynamic/MinJumpToReachEnd.java
+	 */
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[] input;
+	static int[] a;
+	static int n;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
+		input = new int[n];
+		a = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+			a[i] = i;
+		}
+
+		for (int i = 1; i < n; i++)
+			for (int j = 0; j < i; j++)
+				if (i - j <= input[j])
+					a[i] = Math.min(a[i], a[j] + 1);
+
+		System.out.println(a[n - 1]);
+	}
+
+}
