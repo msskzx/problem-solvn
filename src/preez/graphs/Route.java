f9bc7165f23bd3bd4cf53221a97e576cad856818
Muhammad Khattab <msskmohammed96@gmail.com>
2016-09-15 16:47:42 +0200

    vertix isRechable

--- src/preez/graphs/Route.java
+++ src/preez/graphs/Route.java
@@ -1,35 +1,79 @@
 package preez.graphs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Route {
 
-	static Scanner sc = new Scanner(System.in);
-	static int n, s, t;
-	static boolean[] visited;
-	static ArrayList<Integer>[] g;
+	/*
+	 * Given a directed graph, design an algorithm to find out whether there is
+	 * a route between two nodes
+	 * 
+	 * input:
+	 * 
+	 * vertices edges
+	 * 
+	 * Vertex neighbor
+	 * 
+	 * source target
+	 * 
+	 */
 
-	void f() {
-		for(int i = 0;i<n;i++)
-		{
-			while(!g[i].isEmpty()){
-				System.out.println(i+"->"+g[i].remove(0));
+	static Scanner sc = new Scanner(System.in);
+	static int v, e, src, target, cur;
+	static boolean[] visited;
+	static ArrayList<ArrayList<Integer>> g;
+	static Queue<Integer> queue = new LinkedList<>();
+
+	static boolean isReachable(int src, int target) {
+		visited = new boolean[v + 1];
+		if (src == target)
+			return true;
+
+		queue.add(src);
+		visited[src] = true;
+
+		while (!queue.isEmpty()) {
+			src = queue.remove();
+
+			while (!g.get(src).isEmpty()) {
+				cur = g.get(src).remove(0);
+				if (cur == target) {
+					return true;
+				}
+
+				if (!visited[cur]) {
+					queue.add(cur);
+					visited[cur] = true;
+				}
+
 			}
+
 		}
+		return false;
 	}
 
 	public static void main(String[] args) {
-		n = sc.nextInt();
-		g = new ArrayList[n];
-		for (int i = 0; i < n; i++) {
-			s = sc.nextInt();
-			t = sc.nextInt();
-			if (g[s] == null) {
-				g[s] = new ArrayList<Integer>();
-			}
-			g[s].add(t);
+		v = sc.nextInt();
+		e = sc.nextInt();
+		g = new ArrayList<ArrayList<Integer>>();
+
+		for (int i = 0; i < v + 1; i++)
+			g.add(new ArrayList<Integer>());
+
+		for (int i = 0; i < e; i++) {
+			src = sc.nextInt();
+			target = sc.nextInt();
+			g.get(src).add(target);
 		}
+
+		src = sc.nextInt();
+		target = sc.nextInt();
+
+		System.out.println(isReachable(src, target));
+
 	}
 
 }
