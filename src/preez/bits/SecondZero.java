1ec09d42d59ac0580f58fc3a9063f424b4df7a70
Muhammad Khattab <msskmohammed96@gmail.com>
2016-10-18 11:31:57 +0200

    swap pairs, prefix evaluation

--- src/preez/bits/SecondZero.java
+++ src/preez/bits/SecondZero.java
@@ -1,8 +1,11 @@
-package preez.implementation;
+package preez.bits;
 
 public class SecondZero {
 
 	/*
+	 * https://codefights.com/arcade/code-arcade/corner-of-0s-and-1s/
+	 * 9nSj6DgqLDsBePJha
+	 * 
 	 * Presented with the integer n, find the 0-based position of the second
 	 * rightmost zero bit in its binary representation (it is guaranteed that
 	 * such a bit exists), counting from right to left.
