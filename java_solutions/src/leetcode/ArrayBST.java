0132920d3a84529b3fd0be588de3986139f0db8a
Muhammad Khattab <msskzx@gmail.com>
2024-08-10 10:12:22 +0200

    sorted array to bst

---
+++ java_solutions/src/leetcode/ArrayBST.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return this.dfs(nums, 0, nums.length - 1);
+    }
+
+    TreeNode dfs(int[] nums, int i, int j) {
+        if (i > j) {
+            return null;
+        }
+        int m = (int)(i + j) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = dfs(nums, i, m - 1);
+        root.right = dfs(nums, m + 1, j);
+        return root;
+    }
+}
