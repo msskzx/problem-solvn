581f79e1335999b6957d2264ba20bc7fdbab55e9
Muhammad Khattab <msskzx@gmail.com>
2024-08-20 10:42:32 +0200

    pal linked list

---
+++ java_solutions/src/leetcode/linkedli/PalindromeLinkedList.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode tail = this.rev(slow);
+        
+        while (tail != null) {
+            if (head.val != tail.val) {
+                return false;
+            }
+            head = head.next;
+            tail = tail.next;
+        }
+        return true;
+        
+    }
+
+    ListNode rev(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode t = head.next;
+            head.next = prev;
+            prev = head;
+            head = t;
+        }
+        return prev;
+    }
+
+}
